1b4ec0ab1a6aa5b5e5e7408890683528400c09a8
Ana Perez Santos <a.jperez@yahoo.com>
2019-10-23 09:50:28 -0600

    Add tests and method for add_exhibit

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,7 +1,13 @@
 class Museum
-  attr_reader :name
+  attr_reader :name, :exhibits
 
   def initialize(museum_name)
     @name = museum_name
+    @exhibits = []
   end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
 end
--- test/museum_test.rb
+++ test/museum_test.rb
@@ -7,6 +7,9 @@ require './lib/patron'
 class MuseumTest < Minitest::Test
   def setup
     @dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15)
   end
 
   def test_it_exists
@@ -16,5 +19,18 @@ class MuseumTest < Minitest::Test
   def test_it_has_name
     assert_equal "Denver Museum of Nature and Science", @dmns.name
   end
+
+  def test_it_can_hold_exhibits
+    assert_equal [], @dmns.exhibits
+  end
+
+  def test_it_can_add_exhibits
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
 
 end
