f53bd99aa11a6ba464a382347ae5c9ea914d06e1
Ana Perez Santos <a.jperez@yahoo.com>
2019-10-23 09:43:27 -0600

    Add class and initial tests for museum

---
+++ lib/museum.rb
@@ -0,0 +1,7 @@
+class Museum
+  attr_reader :name
+
+  def initialize(museum_name)
+    @name = museum_name
+  end
+end
---
+++ test/museum_test.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/museum'
+require './lib/exhibit'
+require './lib/patron'
+
+class MuseumTest < Minitest::Test
+  def setup
+    @dmns = Museum.new("Denver Museum of Nature and Science")
+  end
+
+  def test_it_exists
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_it_has_name
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+  end
+
+end
